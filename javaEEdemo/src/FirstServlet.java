4978be309fa091f06151a260710f20f4e9be05d7
SmokedKoala <k.azarov@inbox.ru>
2021-06-15 23:52:38 +0300

    First program
    program with simple servlet with get responce

---
+++ javaEEdemo/src/FirstServlet.java
@@ -0,0 +1,23 @@
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "FirstServlet", value = "/FirstServlet")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter printWriter = response.getWriter();
+
+        printWriter.println("<html>");
+        printWriter.println("<h1> Hello World! </h1>");
+        printWriter.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
